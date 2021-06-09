a940b427f9320f56d545a8409087eaf2f1ec0140
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-06-09 16:34:13 +0900

    js / Obejct toString

---
+++ type/3-type.js
@@ -0,0 +1,4 @@
+console.log(Object.prototype.toString.call(null));
+//type of 로는 null을 구분할 수 없다 Obejct.prototype.toString을 이용해서 null check 가능
+console.log(typeof []);
+console.log(Object.prototype.toString.call([]));
